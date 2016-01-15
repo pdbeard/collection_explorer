0739d09348c9a0aee7204c2f6a7fc9b494e6e5e6
pdbeard <pdbeard@iu.edu>
2016-01-15 14:40:37 -0500

    fixed timer issues for multiple slideshows

--- js/index.js
+++ js/index.js
@@ -1,52 +1,58 @@
 (function() {
-  var bHeight, bWidth, block, bname, closeBtn, closeContent, content, expand, openContent, updateValues, wHeight, wWidth, xVal, yVal;
+  var bHeight, bWidth, block, bname, closeBtn, closeContent, content, expand, openContent, updateValues, wHeight, wWidth, xVal, yVal, num, aContent, slideTime;
 
   block = $('.blocks__block');
-
   bname = $('.blocks__name');
-
   content = $('.blocks-content__content');
-
   closeBtn = $('.blocks__content-close');
-
   wHeight = $(window).outerHeight();
-
   wWidth = $(window).outerWidth();
-
   bHeight = block.outerHeight();
-
   bWidth = block.outerWidth();
-
   xVal = Math.round(wWidth / bWidth) + 0.03;
-
   yVal = wHeight / bHeight + 0.03;
 
-  expand = function() {
-	var aBlock, num;
+  expand = function()
+  {
+	var aBlock;
 	num = $(this).index();
 	aBlock = block.eq(num);
-	if (!aBlock.hasClass('active')) {
+	if (!aBlock.hasClass('active'))
+	{
 	  bname.css('opacity', '0');
 	  aBlock.css({
 		'transform': 'scale(' + xVal + ',' + yVal + ')',
 		'-webkit-transform': 'scale(' + xVal + ',' + yVal + ')'
 	  });
 	  aBlock.addClass('active');
-	  openContent(num);
+	  openContent();
 	}
   };
 
-  openContent = function(num) {
-	var aContent;
+  openContent = function()
+  {
 	content.css({
 	  'transition': 'all 0.3s 0.4s ease-out',
 	  '-webkit-transition': 'all 0.3s 0.4s ease-out'
 	});
 	aContent = content.eq(num);
 	aContent.addClass('active');
+	console.log(aContent);
+	aContent.children(0).addClass('active');
+
+	$(".slideshow.active > div:gt(0)").hide();
+	slideTime = setInterval(function() {
+	$('.slideshow.active > div:first')
+		.fadeOut(1000)
+		.next()
+		.fadeIn(1000)
+		.end()
+		.appendTo('.slideshow.active');
+	},  3000);
   };
 
-  closeContent = function() {
+  closeContent = function()
+  {
 	bname.css('opacity', '1');
 	content.css({
 	  'transition': 'all 0.1s 0 ease-out',
@@ -58,9 +64,12 @@
 	});
 	block.removeClass('active');
 	content.removeClass('active');
+	aContent.children(0).removeClass('active');
+	clearInterval(slideTime);
   };
 
-  updateValues = function() {
+  updateValues = function()
+  {
 	var yVal;
 	var xVal;
 	var bWidth;
@@ -83,10 +92,9 @@
 	}
   };
 
+
   $(window).on('resize', updateValues);
-
   bname.on('click', expand);
-
   closeBtn.on('click', closeContent);
 
 }).call(this);
