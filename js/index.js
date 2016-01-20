ac696f6af55707408551ef40fd86de112b9369c2
pdbeard <pdbeard@iu.edu>
2016-01-20 15:18:17 -0500

    Added JSON and img src switch

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 (function() {
-  var bHeight, bWidth, block, bname, closeBtn, closeContent, content, expand, openContent, updateValues, wHeight, wWidth, xVal, yVal, num, aContent, slideTime, autoPick;
+  var bHeight, bWidth, block, bname, closeBtn, closeContent, content, expand, openContent, updateValues, wHeight, wWidth, xVal, yVal, num, aContent, slideTime, autoPick,autoNum, totalNum,autoRun;
 
+  autoNum = 0;
   block = $('.blocks__block');
   bname = $('.blocks__name');
   content = $('.blocks-content__content');
@@ -12,16 +13,25 @@
   xVal = Math.round(wWidth / bWidth) + 0.03;
   yVal = wHeight / bHeight + 0.03;
 
-  var autoRun = 0;
-  var autoNum = 0;
+  totalNum = 6; // Number of Categories
+
+  autoRun = 0;  // 1 for autorun
+				// 0 for interation
 
 
   expand = function()
   {
 	var aBlock;
 
-	if(autoRun ==0){num = $(this).index();}
-	else if (autoRun ==1){num = autoNum;}
+	if(autoRun ==0){
+		num = $(this).index();
+		console.log('You picked category '+num);
+	}
+	else if (autoRun ==1){
+		num = autoNum;
+
+		console.log('Auto picked category '+num);
+	}
 
 	aBlock = block.eq(num);
 
@@ -35,7 +45,6 @@
 	  aBlock.addClass('active');
 	  openContent();
 	}
-	console.log(num);
   };
 
   openContent = function()
@@ -44,23 +53,40 @@
 	  'transition': 'all 0.3s 0.4s ease-out',
 	  '-webkit-transition': 'all 0.3s 0.4s ease-out'
 	});
-	aContent = content.eq(num);
-	aContent.addClass('active');
-	aContent.children(0).addClass('active');
 
-	$(".slideshow.active > div:gt(0)").hide();
-	slideTime = setInterval(function() {
-	$('.slideshow.active > div:first')
-		.fadeOut(1000)
-		.next()
-		.fadeIn(1000)
-		.end()
-		.appendTo('.slideshow.active');
-	},  3000);
+	aContent = content.eq(num);
+	var slideShow = aContent.children(0).prop('id');
+	var slides = slideList[0]["slideshows"][slideShow];
+	var i =0;
+
+	aContent.addClass('active');
+
+	console.log('----------');
+	console.log('Playing: '+aContent.children(0).prop('id'));
+	console.log('Which includes '+slides.length+' images');
+	console.log('----------');
+
+	slideTime = setInterval(function()
+	{
+		$("#"+slideShow).attr("src",slides[i].address);
+		console.log(slides[i].address);
+
+		if(i < slides.length - 1){
+			i++;
+		}
+		else{
+			i=0;
+			console.log('Restart Loop');
+		}
+	},  5000);
   };
 
   closeContent = function()
   {
+	if (aContent !=null){
+		$("#"+aContent.children(0).prop('id')).attr("src","");
+	}
+
 	bname.css('opacity', '1');
 	content.css({
 	  'transition': 'all 0.1s 0 ease-out',
@@ -73,9 +99,6 @@
 	block.removeClass('active');
 	content.removeClass('active');
 
-	if (aContent !=null){
-		aContent.children(0).removeClass('active');
-	}
 
 	clearInterval(slideTime);
   };
@@ -104,31 +127,31 @@
 	}
   };
 
-if(autoRun == 0)
-{
-  $(window).on('resize', updateValues);
-  bname.on('click', expand);
-  closeBtn.on('click', closeContent);
-}
-else if (autoRun ==1)
-{
-
-	autoPick = setInterval(function(){
-		autoRotate();
-	},10000);
-
-	autoRotate = function()
+	if(autoRun == 0)
 	{
-		console.log('AutoPick');
-		closeContent();
-		setTimeout(function(){expand()}, 2000);
-		if (autoNum < 6){
-			autoNum++;
-		}else{
+	  $(window).on('resize', updateValues);
+	  bname.on('click', expand);
+	  closeBtn.on('click', closeContent);
+	}
+	else if (autoRun ==1)
+	{
+		autoPick = setInterval(function(){
+			autoRotate();
+		},2000);
+
+		autoRotate = function()
+		{
+
 			closeContent();
-			autoNum=0;
+			//setTimeout(function(){expand()}, 2000);
+			expand();
+			if (autoNum < totalNum-1){
+				autoNum++;
+			}else{
+				closeContent();
+				autoNum=0;
+			}
 		}
 	}
-}
 
 }).call(this);
